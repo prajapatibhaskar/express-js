e24e3c86b64522e5ec0a607dc86120bebf49f024
Bhaskar Prajapati <prajapatibhaskarbp@gmail.com>
2024-01-16 11:47:35 +0530

    Get Requests

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,43 @@
-import express from 'express';
+import express from "express";
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(201).send({ message: "Hello" });
+});
+
+app.get("/api/users", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      username: "bhaskar",
+      displayName: "bhaskar",
+    },
+    {
+      id: 2,
+      username: "adam",
+      displayName: "adam",
+    },
+    {
+      id: 3,
+      username: "jack",
+      displayName: "jack",
+    },
+  ]);
+});
+
+app.get("/api/products", (req, res) => {
+  res.send([
+    {
+      id: 123,
+      name: "Chicken",
+      price: "12.99",
+    },
+  ]);
+});
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
-})
+  console.log(`Running on port ${PORT}`);
+});
