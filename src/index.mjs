e8126bad5d388d144555f2a527dafb09f8f792d9
Bhaskar Prajapati <prajapatibhaskarbp@gmail.com>
2024-01-17 18:33:03 +0530

    Cookies management in Express

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,14 @@
 import express from "express";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Middleware to parse JSON in the request body
 app.use(express.json());
+
+// Middleware to parse COOKIE
+app.use(cookieParser("helloworld"));
 
 app.use(routes);
 
@@ -12,6 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 // 1. Home page
 app.get("/", (req, res) => {
+  res.cookie("Hello", "World", { maxAge: 30000, signed: true });
   res.status(201).send({ message: "Hello" });
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -1,13 +1,19 @@
 import { Router } from "express";
 
 const router = Router();
-
 router.get("/api/products", (req, res) => {
-  res.send({
-    id: 123,
-    name: "Chicken",
-    price: 12.99,
-  });
+
+  if (req.signedCookies.Hello && req.signedCookies.Hello === "World") {
+    return res.send({
+      id: 123,
+      name: "Chicken",
+      price: 12.99,
+    });
+  }
+
+  return res.status(401).send({
+    msg: 'Sorry, it seems the cookie has expired or you need the correct cookie'
+  })
 });
 
 export default router;
